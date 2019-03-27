8cb5d7d29a0da877b8621d1cd869a466cb393296
Minsu Yun <yms0214@gmail.com>
2019-03-27 21:21:56 +0900

    Kakao Cloning

---
+++ Cloning/pos/pos-self/data.js
@@ -0,0 +1,79 @@
+var meal = [
+  {
+    id: 1,
+    type: 'meal',
+    name: '백반',
+    price: 6000
+  },
+  {
+    id: 2,
+    type: 'meal',
+    name: '불고기정식',
+    price: 7000
+  },
+  {
+    id: 3,
+    type: 'meal',
+    name: '두루치기정식',
+    price: 7000
+  }
+];
+
+var sideMenu = [
+  {
+    id: 1,
+    type: 'sideMenu',
+    name: '삼겹살(100g)',
+    price: 8000
+  },
+  {
+    id: 2,
+    type: 'sideMenu',
+    name: '닭도리탕',
+    price: 20000
+  },
+  {
+    id: 3,
+    type: 'sideMenu',
+    name: '오리백숙',
+    price: 30000
+  },
+  {
+    id: 4,
+    type: 'sideMenu',
+    name: '김치전골',
+    price: 18000
+  }
+];
+
+var drinkMenu = [
+  {
+    id: 1,
+    type: 'drink',
+    name: '소주',
+    price: 4000
+  },
+  {
+    id: 2,
+    type: 'drink',
+    name: '맥주',
+    price: 4000
+  },
+  {
+    id: 3,
+    type: 'drink',
+    name: '막걸리',
+    price: 3500
+  },
+  {
+    id: 4,
+    type: 'drink',
+    name: '음료',
+    price: 1000
+  }
+];
+
+var cart = [
+]
+
+
